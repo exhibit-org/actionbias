2de38d8ecbe6e96635bfd5af9b546fef5c1efa8b
Ben Nevile <ben.nevile@learned-hand.ai>
2025-06-24 11:08:43 -0400

    Add migration note for family terminology column rename

--- db/migrations/0007_family_terminology.sql
+++ db/migrations/0007_family_terminology.sql
@@ -1,3 +1,4 @@
 -- Rename parent columns to family terminology
+-- NOTE: After running this migration, update schema.ts to use 'family_context_summary' and 'family_vision_summary'
 ALTER TABLE "actions" RENAME COLUMN "parent_context_summary" TO "family_context_summary";
 ALTER TABLE "actions" RENAME COLUMN "parent_vision_summary" TO "family_vision_summary";
